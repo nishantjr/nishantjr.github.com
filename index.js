1b3565cdb17e71ce76e07cf2b7805afef8739beb
Nishant Rodrigues <nishantjr@gmail.com>
2016-04-06 01:06:45 +0800

    index.js: Use metalsmith-markdownit to convert the md files to html.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const Metalsmith = require('metalsmith')
+const Metalsmith = require('metalsmith'),
+      markdown   = require('metalsmith-markdownit')
 
 Metalsmith(__dirname)
   .destination('.build/www')
+  .use(markdown())
   .build(function(err) {
     if (err) throw err;
       console.log('Done.');
