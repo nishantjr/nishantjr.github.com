7036244dece901f5778624fc07bb157da26790f6
Nishant Rodrigues <nishantjr@gmail.com>
2017-08-07 00:04:21 -0500

    Gulpfile: Use metalsmith's own serve and watch functionality
    This is instead of using `gulp-connect`.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,20 @@
 'use strict'
 
 const gulp = require('gulp'),
-      connect = require('gulp-connect'),
-      metalsmith = require('./lib/site')
+      metalsmith = require('./lib/site'),
+      serve = require('metalsmith-serve'),
+      watch = require('metalsmith-watch')
 
-gulp.task('build', (cb) => {
-    metalsmith.build(function(err) {
+function build(metalsmith, cb) {
+  metalsmith.build(function(err) {
       if (err) throw err;
       cb();
   })
-});
+}
 
+gulp.task('build', (cb) => { build(metalsmith, cb) });
 gulp.task('serve', ['build'], (cb) => {
-    connect.server({
-        root: metalsmith._destination
-    });
+    build(metalsmith.use(serve({ host: '0.0.0.0', port: 8080, verbose: true }))
+                    .use(watch({ pattern: '**/*', livereload: true })),
+          cb)
 })
